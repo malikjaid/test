a00d351fb0713b63c524f030e2b948fc19a031fd
Junaid Malik <86103833+malikjaid@users.noreply.github.com>
2024-06-13 18:22:56 +0530

    Update increment_version.sh

--- increment_version.sh
+++ increment_version.sh
@@ -6,24 +6,18 @@ git fetch --tags
 # Get the latest tag (version)
 LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-# Default to v1.0.0.0 if no tags exist
-if [ -z "$LATEST_TAG" ]; then
-    LATEST_TAG="v1.0.0.0"
-fi
-
 # Extract the version numbers from the tag
 IFS='.' read -r -a VERSION_PARTS <<< "${LATEST_TAG:1}"
 
 MAJOR=${VERSION_PARTS[0]}
 MINOR=${VERSION_PARTS[1]}
 PATCH=${VERSION_PARTS[2]}
-BUILD=${VERSION_PARTS[3]}
 
-# Increment the build version
-BUILD=$((BUILD + 1))
+# Increment the patch version
+PATCH=$((PATCH + 1))
 
 # Form the new version string
-NEW_VERSION="$MAJOR.$MINOR.$PATCH.$BUILD"
+NEW_VERSION="$MAJOR.$MINOR.$PATCH"
 
 # Create the new tag
 NEW_TAG="v$NEW_VERSION"
@@ -31,5 +25,10 @@ NEW_TAG="v$NEW_VERSION"
 # Commit and tag the new version
 git config user.email "conventional.changelog.action@github.com"
 git config user.name "Conventional Changelog Action"
-git tag
+git tag -a "$NEW_TAG" -m "$NEW_TAG"
+git push origin "$NEW_TAG"
+
+# Create a new release
+RELEASE_BODY=$(conventional-changelog -p angular -i CHANGELOG.md -s -r 0)
+gh release create "$NEW_TAG" --notes "$RELEASE_BODY"
 
