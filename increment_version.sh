cfbbd73b73279b115e9f764f2289b0d17ccea06c
Junaid Malik <86103833+malikjaid@users.noreply.github.com>
2024-08-09 11:03:18 +0530

    Update increment_version.sh

--- increment_version.sh
+++ increment_version.sh
@@ -10,7 +10,7 @@ CURRENT_BRANCH=$(git branch --show-current)
 case "$CURRENT_BRANCH" in
     "main")
         VERSION_FILE="version-main.php"
-        INITIAL_VERSION="11111.0.1"
+        INITIAL_VERSION=""
         ;;
     "malikt")
         VERSION_FILE="version-malikt.php"
